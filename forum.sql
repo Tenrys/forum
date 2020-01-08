3108dd0dbdbbf18dcb3482aed95cb3cb2c7ec846
Marceau MAUBERT <marceau.maubert@laplateforme.io>
2020-01-08 18:44:27 +0100

    Mise à jour sql

--- forum.sql
+++ forum.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 06, 2020 at 07:41 AM
--- Server version: 5.7.18
--- PHP Version: 7.3.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 08, 2020 at 05:34 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,15 +31,17 @@ USE `forum`;
 --
 
 DROP TABLE IF EXISTS `conversations`;
-CREATE TABLE `conversations` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `conversations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `id_auteur` int(11) NOT NULL,
   `id_topic` int(11) NOT NULL,
   `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `verrouillage` tinyint(1) NOT NULL DEFAULT '0',
-  `epingle` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `epingle` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id_topic` (`id_topic`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `conversations`
@@ -49,7 +51,8 @@ INSERT INTO `conversations` (`id`, `nom`, `id_auteur`, `id_topic`, `creation`, `
 (1, 'Premiere conversation', 1, 1, '2019-12-17 19:39:41', 1, 1),
 (2, 'Seconde conversation Ã©ditÃ©e', 1, 1, '2019-12-17 20:13:15', 1, 1),
 (6, 'TroisiÃ¨me topic!', 1, 1, '2020-01-06 07:48:39', 0, 0),
-(7, 'Bonjour !', 2, 4, '2020-01-06 08:38:17', 0, 0);
+(9, 'Conversation crÃ©e par un membre', 2, 1, '2020-01-08 18:18:52', 0, 0),
+(10, 'C\'est pas mal par ici', 3, 4, '2020-01-08 18:21:32', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -58,13 +61,15 @@ INSERT INTO `conversations` (`id`, `nom`, `id_auteur`, `id_topic`, `creation`, `
 --
 
 DROP TABLE IF EXISTS `messages`;
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `contenu` text NOT NULL,
   `id_auteur` int(11) NOT NULL,
   `id_conversation` int(11) NOT NULL,
-  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_conversation` (`id_conversation`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `messages`
@@ -75,7 +80,9 @@ INSERT INTO `messages` (`id`, `contenu`, `id_auteur`, `id_conversation`, `creati
 (6, 'Bruh!!!', 1, 2, '2020-01-05 11:02:44'),
 (8, 'Message de test ...', 1, 6, '2020-01-06 07:48:40'),
 (10, 'Test 123\r\n', 2, 6, '2020-01-06 08:35:13'),
-(11, 'Coucou !!', 2, 7, '2020-01-06 08:38:17');
+(13, 'Bwaah\r\n', 2, 9, '2020-01-08 18:18:52'),
+(14, 'HÃ© oui !', 3, 10, '2020-01-08 18:21:32'),
+(15, 'Un autre message', 2, 10, '2020-01-08 18:23:53');
 
 -- --------------------------------------------------------
 
@@ -84,20 +91,24 @@ INSERT INTO `messages` (`id`, `contenu`, `id_auteur`, `id_conversation`, `creati
 --
 
 DROP TABLE IF EXISTS `reactions`;
-CREATE TABLE `reactions` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `reactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_message` int(11) NOT NULL,
   `type` tinyint(1) NOT NULL,
-  `id_utilisateur` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_message` (`id_message`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `reactions`
 --
 
 INSERT INTO `reactions` (`id`, `id_message`, `type`, `id_utilisateur`) VALUES
-(9, 6, 1, 2),
-(10, 6, 1, 1);
+(12, 6, 1, 1),
+(16, 6, 1, 2),
+(21, 14, 1, 2),
+(22, 15, 0, 2);
 
 -- --------------------------------------------------------
 
@@ -106,12 +117,13 @@ INSERT INTO `reactions` (`id`, `id_message`, `type`, `id_utilisateur`) VALUES
 --
 
 DROP TABLE IF EXISTS `topics`;
-CREATE TABLE `topics` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `rang_min` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `rang_min` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `topics`
@@ -120,7 +132,8 @@ CREATE TABLE `topics` (
 INSERT INTO `topics` (`id`, `nom`, `description`, `rang_min`) VALUES
 (1, 'Test', 'Description', 1),
 (4, 'Visiteurs', 'Bienvenue !', 0),
-(5, 'Admins seulement', 'Top secret!', 3);
+(5, 'Admins seulement', 'Top secret!', 3),
+(6, 'Coucou', 'Toto', 1);
 
 -- --------------------------------------------------------
 
@@ -129,16 +142,17 @@ INSERT INTO `topics` (`id`, `nom`, `description`, `rang_min`) VALUES
 --
 
 DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE `utilisateurs` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `id_rang` int(11) NOT NULL DEFAULT '1',
   `naissance` date DEFAULT NULL,
   `bio` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `utilisateurs`
@@ -146,78 +160,8 @@ CREATE TABLE `utilisateurs` (
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`, `id_rang`, `naissance`, `bio`, `email`, `inscription`) VALUES
 (1, 'admin', '$2y$10$g2nhKiO1qrlEB1SsEPAzYuQ7qU1bI2Tz50i3TCDrk7nd55Tb5nH0y', 3, '1999-11-15', 'Une trÃ¨s longue biographie...', 'test@email.com', '2019-12-17 17:32:50'),
-(2, 'test', '$2y$10$rgotvUSTBv6uZl5I2gLM8Ox4BO6uS4GTXspEd.4XljNO01opkdsjS', 1, NULL, NULL, NULL, '2019-12-17 18:19:49');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `conversations`
---
-ALTER TABLE `conversations`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_topic` (`id_topic`);
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_conversation` (`id_conversation`);
-
---
--- Indexes for table `reactions`
---
-ALTER TABLE `reactions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_message` (`id_message`);
-
---
--- Indexes for table `topics`
---
-ALTER TABLE `topics`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `conversations`
---
-ALTER TABLE `conversations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `reactions`
---
-ALTER TABLE `reactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `topics`
---
-ALTER TABLE `topics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `utilisateurs`
---
-ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+(2, 'test', '$2y$10$rgotvUSTBv6uZl5I2gLM8Ox4BO6uS4GTXspEd.4XljNO01opkdsjS', 1, NULL, NULL, NULL, '2019-12-17 18:19:49'),
+(3, 'enzo', '$2y$10$cD1mowulsxUEelQRX.YuWeeNUZ7igYQrbtYCMH2Kdvg.gm8xZFb6K', 3, NULL, 'Blablabla', NULL, '2020-01-08 18:20:30');
 
 --
 -- Constraints for dumped tables
