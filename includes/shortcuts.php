cbb53feece17665e000ff6777c34c09f9d19d2a2
Tenrys <tenrys.iaido@gmail.com>
2020-01-09 12:16:41 +0100

    Plus rapide

--- includes/shortcuts.php
+++ includes/shortcuts.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new PDO("mysql:host=localhost;dbname=forum", "root", "");
+	$db = new PDO("mysql:host=127.0.0.1;dbname=forum", "root", "");
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 	$rankNames = [
